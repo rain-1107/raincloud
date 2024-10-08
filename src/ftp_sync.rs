5211854c3eab5094b887814cd80b4abdfcc33d9b
rain-1107 <ryan.bays@scchs.org.uk>
2024-10-08 13:15:15 +0100

    fuck ee

--- src/ftp_sync.rs
+++ src/ftp_sync.rs
@@ -7,11 +7,12 @@ use std::{
     f64, fs,
     io::{self, Read, Write},
     path::Path,
+    path::PathBuf,
     result::Result,
     str::from_utf8,
     time::UNIX_EPOCH,
 };
-use zip::{write::FileOptions, CompressionMethod, ZipWriter};
+use zip::{read::ZipFile, write::FileOptions, CompressionMethod, ZipArchive, ZipWriter};
 
 #[derive(serde::Serialize, serde::Deserialize)]
 pub struct SaveData {
@@ -19,6 +20,7 @@ pub struct SaveData {
 }
 
 fn get_filenames(directory: &Path) -> Result<Vec<String>, Box<dyn Error>> {
+    println!("Fetching filenames");
     let mut filenames = Vec::new();
     let paths = fs::read_dir(&directory)?;
     for path_result in paths {
@@ -33,6 +35,7 @@ fn get_filenames(directory: &Path) -> Result<Vec<String>, Box<dyn Error>> {
 }
 
 fn get_max_mod_time(filenames: &Vec<String>) -> Result<f64, Box<dyn Error>> {
+    println!("Fetching modification time");
     let mut max = 0.0;
     for p in filenames {
         let path = Path::new(&p);
@@ -51,16 +54,19 @@ fn get_max_mod_time(filenames: &Vec<String>) -> Result<f64, Box<dyn Error>> {
 
 fn create_zip_archive(
     name: &String,
-    source: &String,
-    destination: &String,
+    srcpath: &PathBuf,
+    destination: &mut PathBuf,
 ) -> Result<(), Box<dyn Error>> {
-    let mut srcpath = std::path::PathBuf::new();
-    srcpath.push(source);
-    let mut tmp = std::path::PathBuf::new();
-    tmp.push(destination);
-    tmp.push(name);
-    let zip_path = fs::File::create(&tmp)?;
+    println!("Creating zip archive for {}", name);
+    destination.push(name);
+    println!(
+        "{}",
+        &destination.clone().into_os_string().into_string().unwrap()
+    );
+    let zip_path = fs::File::create(&destination)?;
+    println!("Created empty file");
     let mut zip_file = ZipWriter::new(zip_path);
+    println!("Started zip");
     let options: zip::write::FileOptions<zip::write::ExtendedFileOptions> =
         FileOptions::default().compression_method(CompressionMethod::DEFLATE);
     let filenames = get_filenames(&srcpath)?;
@@ -77,6 +83,7 @@ fn create_zip_archive(
         zip_file.write_all(&buffer)?;
     }
     zip_file.finish()?;
+    destination.pop();
     Ok(())
 }
 
@@ -88,24 +95,25 @@ pub fn sync_save(
     password: &String,
     port: u16,
 ) -> Result<(), Box<dyn Error>> {
-    println!("Creating zip archive for {}", savename);
     let mut tmp = home::home_dir().unwrap();
     tmp.push(".rc"); // TODO: link this to constant
     tmp.push("tmp");
     if !tmp.exists() {
         fs::create_dir(&tmp)?;
     }
-    let dirpath = Path::new(&directory);
+    let dirpath = Path::new(&directory).to_path_buf();
     if !dirpath.exists() {
         return Ok(());
     }
     // TODO: fixxxxxxxxxxxxxxxxxxxx
-    let filenames = get_filenames(dirpath)?;
+    let filenames = get_filenames(&dirpath)?;
     let max_mod_time = get_max_mod_time(&filenames)?;
     let data = SaveData { time: max_mod_time };
     let j = serde_json::to_string(&data)?;
     tmp.push(savename.to_string() + "-" + &Local::now().date_naive().to_string() + ".json");
     fs::write(&tmp, &j).expect("Unable to write file");
+    tmp.pop();
+    println!("Logging in to FTP server");
     let mut ftp_stream = FtpStream::connect(address.to_string() + ":" + &port.to_string())
         .unwrap_or_else(|err| panic!("{}", err));
     ftp_stream.login(username, password)?;
@@ -129,25 +137,59 @@ pub fn sync_save(
             break;
         }
     }
+    let save_filename: String = savename.to_owned() + "-" + &Local::now().date_naive().to_string();
+
     if json_f == "".to_string() {
-        let zip_name: String =
-            savename.to_owned() + "-" + &Local::now().date_naive().to_string() + ".zip";
-        create_zip_archive(
-            &zip_name,
-            &directory.to_string(),
-            &tmp.clone().into_os_string().into_string().unwrap(),
-        )?;
-        tmp.push(&zip_name);
+        println!("Previous save not found, uploading save");
+        create_zip_archive(&(save_filename.clone() + ".zip"), &dirpath, &mut tmp)?;
+        println!("Zip archive created");
+        tmp.push(&(save_filename.clone() + ".zip"));
         let mut zip_file = fs::File::open(&tmp)?;
-        ftp_stream.put(&zip_name, &mut zip_file)?;
+        tmp.pop();
+        tmp.push(&(save_filename.clone() + ".json"));
+        let json_file_data = serde_json::to_string(&data)?;
+        fs::write(&tmp, &json_file_data)?;
+        println!("Created json file.");
+        let mut json_file = fs::File::open(&tmp)?;
+        ftp_stream.put(&(save_filename.clone() + ".json"), &mut json_file)?;
+        ftp_stream.put(&(save_filename.clone() + ".zip"), &mut zip_file)?;
     } else {
+        println!("Checking date of previous save");
         let cursor = ftp_stream.simple_retr(&json_f)?;
         let vec = cursor.into_inner();
         let file = from_utf8(&vec)?;
-        let data: SaveData = serde_json::from_str(&file)?;
-        println!("{}", data.time);
-        // TODO: Finish logic for deciding to upload or download to sync
+        let server_data: SaveData = serde_json::from_str(&file)?;
+        if server_data.time > data.time {
+            println!("Downloading previous save");
+            tmp.push(&(save_filename.clone() + ".zip"));
+            let mut zip_file = fs::File::create(&tmp)?;
+            let cursor = ftp_stream.simple_retr(&(save_filename.clone() + ".zip"))?;
+            let vec = cursor.into_inner();
+            zip_file.write(&vec)?;
+            let mut zip_archive = ZipArchive::new(&zip_file)?;
+            zip_archive.extract(dirpath)?;
+        } else if server_data.time == data.time {
+            println!("Already up to date.")
+            // Do nothing
+        } else {
+            println!("Uploading local save to cloud");
+            for item in ftp_stream.nlst(None)? {
+                let _ = ftp_stream.rm(&item);
+            }
+            let save_filename: String =
+                savename.to_owned() + "-" + &Local::now().date_naive().to_string();
+            create_zip_archive(&(save_filename.clone() + ".zip"), &dirpath, &mut tmp)?;
+            tmp.push(&(save_filename.clone() + ".zip"));
+            let mut zip_file = fs::File::open(&tmp)?;
+            tmp.pop();
+            tmp.push(&(save_filename.clone() + ".json"));
+            let json_file_data = serde_json::to_string(&data)?;
+            fs::write(&tmp, &json_file_data)?;
+            let mut json_file = fs::File::open(&tmp)?;
+            ftp_stream.put(&(save_filename.clone() + ".json"), &mut json_file)?;
+            ftp_stream.put(&(save_filename.clone() + ".zip"), &mut zip_file)?;
+        }
     }
-    let _ = ftp_stream.quit();
+    ftp_stream.quit()?;
     Ok(())
 }
